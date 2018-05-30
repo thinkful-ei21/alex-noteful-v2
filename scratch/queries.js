e13315c3d8bc5ab5348f9b9798b38e813b677287
alex <alex@localhost.localdomain>
2018-05-30 13:15:02 -0700

    added knex update

--- scratch/queries.js
+++ scratch/queries.js
@@ -2,19 +2,49 @@
 
 const knex = require('../knex');
 
-let searchTerm = 'gaga';
+let searchTerm;
+// knex
+//   .select('notes.id', 'title', 'content')
+//   .from('notes')
+//   .modify(queryBuilder => {
+//     if (searchTerm) {
+//       queryBuilder.where('title', 'like', `%${searchTerm}%`);
+//     }
+//   })
+//   .orderBy('notes.id')
+//   .then(results => {
+//     console.log(JSON.stringify(results, null, 2));
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
+
+// let id = 1
+// knex
+// .select('notes.id', 'title', 'content')
+// .from('notes')
+// .modify(queryBuilder => {
+//   if (id) {
+//     queryBuilder.where({id: id});
+//   }
+// })
+// .then(results => {
+//   console.log(JSON.stringify(results, null, 2));
+// })
+// .catch(err => {
+//   console.error(err);
+// });
+let id = 40;
+let obj = {id: 40, title: 'taco time', content: 'lets tac about tacos'};
+
 knex
-  .select('notes.id', 'title', 'content')
-  .from('notes')
-  .modify(queryBuilder => {
-    if (searchTerm) {
-      queryBuilder.where('title', 'like', `%${searchTerm}%`);
-    }
-  })
-  .orderBy('notes.id')
-  .then(results => {
-    console.log(JSON.stringify(results, null, 2));
-  })
-  .catch(err => {
-    console.error(err);
-  });
+.update(obj)
+.from('notes')
+.where({id: id})
+.returning(['id', 'title', 'content'])
+.then(results => {
+  console.log(JSON.stringify(results[0], null, 2));
+})
+.catch(err => {
+  console.error(err);
+});
